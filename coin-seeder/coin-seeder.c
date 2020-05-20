0ceb0f08cd521c9d7de993a14c737c0b4ab18248
Chapman Shoop <chapman.shoop@gmail.com>
2020-05-20 14:49:17 -0700

    POSIX Threads work, OpenMP would not using build2 :thinking:

--- coin-seeder/coin-seeder.c
+++ coin-seeder/coin-seeder.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
 #include <liblog/log.h>
-
-// DNS Server
-
-// Peercoin Crawler
-//   * Seed node
-//   * Good nodes
-
+#include <stdbool.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -20,11 +16,27 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define BUF_SIZE 500
 
+bool QUIT = false;
 
-char *SEED_NODE = "peercoin.peercoin-library.org";
-char *SEED_PORT = "9901";
+const char *SEED_NODE = "peercoin.peercoin-library.org";
+const char *SEED_PORT = "9901";
+
+pthread_mutex_t SEED_MUTEX;
+int SEED_HEIGHT = 0;
+char *SEED_VERSION = NULL;
+
+typedef struct coin_node {
+    struct sockaddr address;
+    int last_contact;
+    struct coin_node* next;
+} coin_node_s;
+
+pthread_mutex_t GOOD_MUTEX;
+coin_node_s *GOOD_NODES = NULL;
+
+pthread_mutex_t CHECK_MUTEX;
+coin_node_s *CHECK_NODES = NULL;
 
 
 int connect_to_seed_node() {
@@ -71,30 +83,62 @@ int connect_to_seed_node() {
 }
 
 
-void seed_node_thread() {
-    log_trace("seed_node_thread()");
+void *seed_thread(void *vin) {
+    log_trace("Entering seed_node_thread()");
 
-    // Need to make a tcp connection
-    int tcp_socket = connect_to_seed_node();
-    log_debug("Connected to SEED_NODE on socket: %d", tcp_socket);
+    while(!QUIT) {
+        // Need to make a tcp connection
+        int tcp_socket = connect_to_seed_node();
+        log_debug("Connected to SEED_NODE on socket: %d", tcp_socket);
 
-    // Maybe hints on reading / writing to the socket!
+        // Maybe hints on reading / writing to the socket! https://www.geeksforgeeks.org/socket-programming-cc/
 
-    // Need to send some messages
+        // Need to send some messages
 
-    // Need to get block chain height, user agent, version, etc.
+        // Need to get block chain height, user agent, version, etc.
 
-    // Use this info to judge other nodes :)
+        // Use this info to judge other nodes :)
 
-    // Need to get peer addresses
+        // Need to get peer addresses
 
-    // Use this to populate a queue (linked list?)
-    close(tcp_socket);
+        // Use this to populate a queue (linked list?)
+        close(tcp_socket);
+
+        QUIT = true;
+    }
+
+    log_trace("Exiting seed_node_thread()");
+    return NULL;
 }
+
+
+void *dns_thread() {
+    log_trace("Entering dns_server_thread()");
+
+    while(!QUIT) {
+        // Need to listen for incoming UDP requests and respond appropriately...
+        log_info("I AM DNS THREAD LOL");
+    }
+
+    log_trace("Exiting dns_server_thread()");
+    return NULL;
+}
+
+
+// Two ideas, then move on to C++
+// One: capture ctrl-c and shutdown gracefully
+// Two: get the handshake to work?
 
 
 int main (int argc, char *argv[])
 {
     log_set_level(LOG_TRACE);
-    seed_node_thread();
+
+    pthread_t seed, dns;
+    pthread_create(&seed, NULL, &seed_thread, NULL);
+    pthread_create(&dns, NULL, &dns_thread, NULL);
+
+    void *_retval;
+    pthread_join(seed, &_retval);
+    pthread_join(dns, &_retval);
 }
